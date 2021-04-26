a7b6891b039417bc177a32074d492ed5bba863fa
Elliot Revere <82168908+ERevere@users.noreply.github.com>
2021-04-26 20:23:35 +0100

    Update Number.cpp
    Made math more random

--- Number.cpp
+++ Number.cpp
@@ -1,34 +1,37 @@
 
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 
 int
-main ()
+main()
 {
-  int lowerbound, higherbound, Secret, Guess;
+    int lowerbound, higherbound, Secret, Guess;
 
-  cout << "Enter the lowerbound for your random number: ";
-  cin >> lowerbound;
-  lowerbound = lowerbound - 1;
-  cout << "Enter the higherbound for your random number: ";
-  cin >> higherbound;
-  higherbound = higherbound + 1;
-  Secret = rand () % higherbound + lowerbound;
-  cout << "Guess the number which has been generated: ";
-  cin >> Guess;
+    cout << "Enter the lowerbound for your random number: ";
+    cin >> lowerbound;
+    lowerbound = lowerbound - 1;
+    cout << "Enter the higherbound for your random number: ";
+    cin >> higherbound;
+    higherbound = higherbound + 1;
+    Secret = (time(NULL)) % higherbound + lowerbound;
+    cout << "Guess the number which has been generated: ";
+    cin >> Guess;
 
 
-  if (Secret < Guess)
-    cout << "The random number was lower" << " The final number was: " <<
-      Secret << " While your guess was: " << Guess;
+    if (Secret < Guess)
+        cout << "The random number was lower" << " // The final number was: " <<
+        Secret << " // While your guess was: " << Guess;
 
-  if (Secret > Guess)
-    cout << "The random number was higher" << " The final number was: " <<
-      Secret << " While your guess was: " << Guess;
+    if (Secret > Guess)
+        cout << "The random number was higher" << " // The final number was: " <<
+        Secret << " // While your guess was: " << Guess;
 
-  if (Secret == Guess)
-    cout << "Congratualtion, you guess yes right very nice";
+    if (Secret == Guess)
+        cout << "Congratualtion, you guess yes right very nice";
 
-  return 0;
+    return 0;
 }
